01278c79481a29c38be46c3f669e75fbd1180eda
Raza Ali <136004622+RazaAli313@users.noreply.github.com>
2026-03-25 14:10:46 +0500

    Change default server port from 8080 to 80

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const config = require("dotenv").config;
 
 config();
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 80;
 
 app.listen(PORT, () => {
   console.log(`The server is listening on THE port ${PORT}`);
